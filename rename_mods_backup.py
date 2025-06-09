3617496b4a92a7c3c74304b040a9fdd51068c469
YassineHane <yassine.hane@gmail.com>
2025-06-09 19:50:38 +0200

    imporove backup script with logs

--- rename_mods_backup.py
+++ rename_mods_backup.py
@@ -8,12 +8,14 @@ print("renaming folders")
 print(list_folder)
 for folder_name in list_folder:
     renamed_folder = folder_name.replace("@","").lower()
+    print(f"Renaming file {folder_name=} -> {renamed_folder=}")
     shutil.move(src=folder_name, dst=renamed_folder)
 
     # ensure Addons folder exist and rename it to lowercase
     addons_folder_path = Path(renamed_folder)/"Addons"
     if addons_folder_path.is_dir():
-
-        shutil.move(src=addons_folder_path, dst=str(addons_folder_path).replace("Addons", "addons"))
+        renamed_addon_folder = str(addons_folder_path).replace("Addons", "addons")
+        print(f"Renaming folder {addons_folder_path=} -> {renamed_folder=}")
+        shutil.move(src=addons_folder_path, dst=renamed_addon_folder)
 
 print("Done")
